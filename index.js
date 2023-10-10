22be55b4953e31fc8c5a8e2e143b99c653686a46
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-10-09 21:59:21 -0400

    some changes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-/*function hello(name){
+function hello(name){
     const message = "hello "
     return message + name
 }
 console.log(hello("Andy"))
-*/
 
 
-/*
+
+
 function add(num1, num2){
     return num1 + num2
 }
 
 console.log(add(2, 3))
-*/
+
 
 function multiply(num1, num2){
     return num1 * num2
