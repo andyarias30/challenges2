9e6784663fb0c88ff5d625bfaa02357250143982
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-10-09 21:55:27 -0400

    Functions practice

---
+++ index.js
@@ -0,0 +1,27 @@
+/*function hello(name){
+    const message = "hello "
+    return message + name
+}
+console.log(hello("Andy"))
+*/
+
+
+/*
+function add(num1, num2){
+    return num1 + num2
+}
+
+console.log(add(2, 3))
+*/
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+console.log(multiply(67 ,90))
+
+
+function multiply(num1, num2){
+    const num3 = num1 * num2
+    return num1 + ' x ' + num2 + ' = ' +  num3
+}
+console.log(multiply(6, 9))
